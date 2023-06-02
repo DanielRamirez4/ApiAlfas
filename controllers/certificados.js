3e44ca096571b73efbdaf9a959aa892599465cff
Erick Díaz Villegas <Erick Diaz>
2023-06-01 21:11:03 -0600

    Se creo certificadosGet y certificadoPost

--- controllers/certificados.js
+++ controllers/certificados.js
@@ -1,13 +1,32 @@
+const connection = require('../database/config');
+
 const certificadosGet = (req, res) => {
-    res.json('ruta para obtener todos los certificados');
+    //res.json('ruta para obtener todos los certificados');
+    try{
+        connection.query(`SELECT * FROM certificados;`, (err,results) => {
+            res.json(results);
+        })
+    }catch(error){
+        console.log(error);
+    }
 }
 
 const certificadoGet = async(req, res) => {
     res.json('ruta para un certificado');
+    
 }
 
 const certificadoPost = async(req, res) => {
-    res.json('ruta para registrar un certificado');
+    //res.json('ruta para registrar un certificado');
+    try{
+        const {nombre, descripcion, logo} = req.body;
+
+        connection.query(`INSERT INTO certificados(nombre, descripcion, logo) VALUES("${nombre}","${descripcion}","${logo}")`, (err, results) => {
+            res.json(results);
+        });
+    }catch(error){
+        console.log(error);
+    }
 }
 
 module.exports = {
